d4c74e0b5503c5e87dbe0b1aaa9dd199a88cc65d
suhailaaziz <94121635+suhailaaziz@users.noreply.github.com>
2022-09-14 14:57:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $("#signup-form").validate({
+        rules:{
+            fname:{
+                required:true,
+                minlength:4,
+                maxlength:6
+            },
+            sname:{
+                required:true,
+                minlength:4,
+                maxlength:6
+            },
+            Emailaddress:{
+                required:true,
+                email:true
+            },
+            password:{
+                minlength:6
+    
+            },
+            day:{
+                required:true
+            },
+            month:{
+                required:true
+            },
+            year:{
+                required:true
+            },
+            gender:{
+                required:true
+            }
+        },
+        messages:{
+            
+        }
+        
+    })
+})
